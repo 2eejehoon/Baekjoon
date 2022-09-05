2ce4aea7ba8a2020ba60173f971d2a0e76686ece
leejehoon92 <2eejehoon@gmail.com>
2022-09-05 15:05:49 +0900

    baekjoon 2775
    부녀회장이 될테야

--- baekjoon 2775.js
+++ baekjoon 2775.js
@@ -1,24 +1,25 @@
 const fs = require("fs");
-const input = fs.readFileSync("/dev/stdin").toString().split("\n");
-const T = Number(input.shift());
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath).toString().split("\n").map(Number);
+const T = input.shift();
 
-for (let i = 0; i < T; i++) {
-  const k = +input.shift();
-  const n = +input.shift();
-
-  // 각 방당 인원수를 저장할 2차원 배열 선언
-  const house = Array.from(Array(k + 1), () => Array(n + 1).fill(0));
-  // 0층 (0층의 i호에는 i명이 산다.)
-  for (let i = 1; i <= n; i++) {
-    house[0][i] = i;
+for (let i = 0; i < T * 2; i += 2) {
+  let k = input[i];
+  let n = input[i + 1];
+  let arr = [];
+  for (let i = 0; i <= k; i++) {
+    arr[i] = [];
   }
-  // 1 ~ k층 (k층 n호에는 k-1층 n호 + k층 n-1호만큼 산다.)
-  for (let i = 1; i <= k; i++) {
-    for (let j = 1; j <= n; j++) {
-      house[i][j] = house[i - 1][j] + house[i][j - 1];
+  for (let i = 0; i <= k; i++) {
+    sum = 0;
+    for (let j = 0; j < n; j++) {
+      if (i === 0) {
+        arr[i][j] = j + 1;
+      } else {
+        sum = sum + arr[i - 1][j];
+        arr[i][j] = sum;
+      }
     }
   }
-
-  // k층 n호의 인원수 출력
-  console.log(house[k][n]);
+  console.log(arr[k][n - 1]);
 }
